85a7f19ee3ac8ecc522374d54cfe7515eb44516c
Artemio <artemiolucero@gmail.com>
2022-07-15 16:14:04 -0300

    Added Movies controller

---
+++ ApiRestDisney/Controllers/MovieOrSeriesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiRestDisney.DataAccess;
+using ApiRestDisney.Models.DataModels;
+
+namespace ApiRestDisney.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MovieOrSeriesController : ControllerBase
+    {
+        private readonly DisneyDbContext _context;
+
+        public MovieOrSeriesController(DisneyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MovieOrSeries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MovieOrSeries>>> GetMovieOrSeries()
+        {
+          if (_context.MovieOrSeries == null)
+          {
+              return NotFound();
+          }
+            return await _context.MovieOrSeries.ToListAsync();
+        }
+
+        // GET: api/MovieOrSeries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MovieOrSeries>> GetMovieOrSeries(int id)
+        {
+          if (_context.MovieOrSeries == null)
+          {
+              return NotFound();
+          }
+            var movieOrSeries = await _context.MovieOrSeries.FindAsync(id);
+
+            if (movieOrSeries == null)
+            {
+                return NotFound();
+            }
+
+            return movieOrSeries;
+        }
+
+        // PUT: api/MovieOrSeries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovieOrSeries(int id, MovieOrSeries movieOrSeries)
+        {
+            if (id != movieOrSeries.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movieOrSeries).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieOrSeriesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MovieOrSeries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<MovieOrSeries>> PostMovieOrSeries(MovieOrSeries movieOrSeries)
+        {
+          if (_context.MovieOrSeries == null)
+          {
+              return Problem("Entity set 'DisneyDbContext.MovieOrSeries'  is null.");
+          }
+            _context.MovieOrSeries.Add(movieOrSeries);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovieOrSeries", new { id = movieOrSeries.Id }, movieOrSeries);
+        }
+
+        // DELETE: api/MovieOrSeries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovieOrSeries(int id)
+        {
+            if (_context.MovieOrSeries == null)
+            {
+                return NotFound();
+            }
+            var movieOrSeries = await _context.MovieOrSeries.FindAsync(id);
+            if (movieOrSeries == null)
+            {
+                return NotFound();
+            }
+
+            _context.MovieOrSeries.Remove(movieOrSeries);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieOrSeriesExists(int id)
+        {
+            return (_context.MovieOrSeries?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ApiRestDisney/Migrations/20220715191156_All Tables.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiRestDisney.Migrations
+{
+    public partial class AllTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<float>(type: "real", nullable: false),
+                    History = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieOrSeries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Calification = table.Column<int>(type: "int", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieOrSeries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovieOrSeries_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterMovieOrSeries",
+                columns: table => new
+                {
+                    CharactersId = table.Column<int>(type: "int", nullable: false),
+                    MoviesOrSeriesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterMovieOrSeries", x => new { x.CharactersId, x.MoviesOrSeriesId });
+                    table.ForeignKey(
+                        name: "FK_CharacterMovieOrSeries_Characters_CharactersId",
+                        column: x => x.CharactersId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CharacterMovieOrSeries_MovieOrSeries_MoviesOrSeriesId",
+                        column: x => x.MoviesOrSeriesId,
+                        principalTable: "MovieOrSeries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterMovieOrSeries_MoviesOrSeriesId",
+                table: "CharacterMovieOrSeries",
+                column: "MoviesOrSeriesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieOrSeries_GenreId",
+                table: "MovieOrSeries",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterMovieOrSeries");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Characters");
+
+            migrationBuilder.DropTable(
+                name: "MovieOrSeries");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
